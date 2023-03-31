8d12c64e38e90e1a448676ee0e0b0dd37f3c8ca0
Bhanu5302 <98444997+Bhanu5302@users.noreply.github.com>
2023-03-31 13:46:00 +0530

    Update node-red.sh

--- node-red.sh
+++ node-red.sh
@@ -4,3 +4,10 @@
 user_name=$(whoami)
 echo "#######################################################################################################################"
 echo ">>Nodejs14.x & Node-red installation started"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm install v14.10.0
+echo $nvm list
+sudo npm install -g --unsafe-perm node-red
+echo $node-red -v
