0b4565994916d96aa2726d53554c192a2eb864e3
Bhanu5302 <98444997+Bhanu5302@users.noreply.github.com>
2023-03-31 13:41:20 +0530

    Create node-red.sh

---
+++ node-red.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eu
+# nodejs14.x & node-red instllation instllation 2-01-2023 bhanu prakash
+
+user_name=$(whoami)
+echo "#######################################################################################################################"
+echo ">>Nodejs14.x & Node-red installation started"
