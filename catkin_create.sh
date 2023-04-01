6cd9b7d06fc060609354e51184fcaa83fcbc15d4
Bhanu5302 <98444997+Bhanu5302@users.noreply.github.com>
2023-04-01 14:19:54 +0530

    Update catkin_create.sh

--- catkin_create.sh
+++ catkin_create.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -eu
+echo "creating workspace"
 source /opt/ros/noetic/setup.bash
 mkdir -p ~/nala_moco/src
 cd ~/nala_moco/
 catkin_make
-source devel/setup.bash
+source ~/nala_moco/devel/setup.bash
 echo $ROS_PACKAGE_PATH
+echo ">>>>>downloading the moco2.1 project-----"
 cd ~/nala_moco/src
-gitclone https://github.com/Bhanu5302/moco2.1.git
+git clone https://github.com/Bhanu5302/moco2.1.git
 cd ~/nala_moco
-source devel/setup.bash
 catkin_make
-cd /etc
-
+source ~/nala_moco/devel/setup.bash
+echo "source ~/nala_moco/devel/setup.bash">>~/.bashrc
+source ~/.bashrc
+echo "Downloaded moco2.1"
+sudo apt-get install busybox
+sudo apt-get install pip
+pip install python-can
