6eb95339432ed0483c0f1a101f372b97d92bb904
Bhanu5302 <98444997+Bhanu5302@users.noreply.github.com>
2023-04-03 14:20:45 +0530

    Update node-red.sh

--- node-red.sh
+++ node-red.sh
@@ -7,5 +7,6 @@ echo ">>Nodejs14.x & Node-red installation started"
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 cat /etc/apt/sources.list.d/nodesource.list
 sudo apt -y install nodejs
+node  -v
 sudo npm install -g --unsafe-perm node-red
 node-red -v
